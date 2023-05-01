cc29dc29963a27bf31630293129ff65d4a56285d
kirlua <98923556+kirlua@users.noreply.github.com>
2023-05-01 17:56:50 +1200

    ff
    ff

---
+++ list_of_monstercard.py
@@ -0,0 +1,31 @@
+import easygui
+
+# writing about dictionary of monstercard game
+monsters = {
+    "Stoneling": {"Strength": 7, "Speed": 1, "Stealth": 25, "Cunning": 15},
+    "Vexscream": {"Strength": 1, "Speed": 6, "Stealth": 21, "Cunning": 19},
+    "Dawnmirage": {"Strength": 5, "Speed": 15, "Stealth": 18, "Cunning": 22},
+    "Blazegolem": {"Strength": 15, "Speed": 20, "Stealth": 23, "Cunning": 6},
+    "Websnake": {"Strength": 7, "Speed": 15, "Stealth": 10, "Cunning": 5},
+    "Moldvine": {"Strength": 21, "Speed": 18, "Stealth": 14, "Cunning": 5},
+    "Vortexwing": {"Strength": 19, "Speed": 13, "Stealth": 19, "Cunning": 2},
+    "Rotthing": {"Strength": 16, "Speed": 7, "Stealth": 4, "Cunning": 12},
+    "Froststep": {"Strength": 14, "Speed": 14, "Stealth": 17, "Cunning": 4},
+    "Wispghoul": {"Strength": 17, "Speed": 19, "Stealth": 3, "Cunning": 2},
+}
+
+
+# showing the list of the monsters that are currently in the list
+def display_monsters():
+    msg = "List of Monster cards: \n"
+    for item, stat in monsters.items():
+        msg += f" {item}: \n"
+        for monster_ability in stat:
+            msg += f'  {monster_ability }: {stat[monster_ability]}\n'
+
+
+    # 그리고 추가했던 msg를 텍스트로 불러온다.
+    easygui.msgbox(msg, title="Monster cards")
+    # gets the name of the monsters
+
+display_monsters()
---
+++ list_of_monstercard2.py
@@ -0,0 +1,64 @@
+import easygui
+monsters_0 = {1:
+          {"Monstername": "Stoneling",
+          "stats": "Strength: 7",
+          "stats": "Speed 1",
+          "stats": "Stealth 25",
+          "stats": "Cunning 15"},}
+monsters_1 = {2:
+          {"Monstername": "Vexscream",
+          "stats": "Strength: 1",
+          "stats": "Speed: 6",
+          "stats": "Stealth: 21",
+          "stats": "Cunning: 19"},}
+monsters_2 = {3:
+          {"Monstername": "Dawnmirage",
+          "stats": "Strength: 5",
+          "stats": "Speed: 15",
+          "stats": "Stealth: 18",
+          "stats": "Cunning: 22"},}
+monsters_3 = {4:
+          {"Monstername": "Blazegolem",
+          "stats": "Strength: 15",
+          "stats": "Speed: 20",
+          "stats": "Stealth: 23",
+          "stats": "Cunning: 6"},}
+monsters_4 = {5:
+          {"Monstername": "Websnake",
+          "stats": "Strength: 7",
+          "stats": "Speed: 15",
+          "stats": "Stealth: 10",
+          "stats": "Cunning: 5"},}
+monsters_5 = {6:
+          {"Monstername": "Moldvine",
+          "stats": "Strength: 21",
+          "stats": "Speed: 18",
+          "stats": "Stealth: 14",
+          "stats": "Cunning: 5"},}
+monsters_6 = {7:
+          {"Monstername": "Vortexwing",
+          "stats": "Strength: 19",
+          "stats": "Speed: 13",
+          "stats": "Stealth: 19",
+          "stats": "Cunning: 2"},}
+monsters_7 = {8:
+          {"Monstername": "Rotthing",
+          "stats": "Strength: 16",
+          "stats": "Speed: 7",
+          "stats": "Stealth: 4",
+          "stats": "Cunning: 12"},}
+monsters_8 = {9:
+          {"Monstername": "Froststep",
+          "stats": "Strength: 14",
+          "stats": "Speed: 14",
+          "stats": "Stealth: 17",
+          "stats": "Cunning: 4"},}
+monsters_9 = {10:
+          {"Monstername": "Wispghoul",
+          "stats": "Strength: 17",
+          "stats": "Speed: 19",
+          "stats": "Stealth: 3",
+          "stats": "Cunning: 2"},}
+
+def display_monsters():
+
