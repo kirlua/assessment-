21b68c9e72cc7540df7af9ef10b9a165929c5e19
kirlua <98923556+kirlua@users.noreply.github.com>
2023-05-08 09:58:07 +1200

    ff
    ff

---
+++ 03_removing_monster_from_list_v1.py
@@ -0,0 +1,21 @@
+monsters = {
+    "Stoneling": {"Strength": 7, "Speed": 1, "Stealth": 25, "Cunning": 15},
+    "Vexscream": {"Strength": 1, "Speed": 6, "Stealth": 21, "Cunning": 19},
+    "Dawnmirage": {"Strength": 5, "Speed": 15, "Stealth": 18, "Cunning": 22},
+    "Blazegolem": {"Strength": 15, "Speed": 20, "Stealth": 23, "Cunning": 6},
+    "Websnake": {"Strength": 7, "Speed": 15, "Stealth": 10, "Cunning": 5},
+    "Moldvine": {"Strength": 21, "Speed": 18, "Stealth": 14, "Cunning": 5},
+    "Vortexwing": {"Strength": 19, "Speed": 13, "Stealth": 19, "Cunning": 2},
+    "Rotthing": {"Strength": 16, "Speed": 7, "Stealth": 4, "Cunning": 12},
+    "Froststep": {"Strength": 14, "Speed": 14, "Stealth": 17, "Cunning": 4},
+    "Wispghoul": {"Strength": 17, "Speed": 19, "Stealth": 3, "Cunning": 2},
+}
+
+print(monsters)
+monsters_to_remove = input("Enter the name of the monster to remove: ")
+if monsters_to_remove in monsters:
+    del monsters[monsters_to_remove]
+    print(f"{monsters_to_remove} has been removed from the list")
+else:
+    print(f"{monsters_to_remove} is not in the list")
+print(monsters)
